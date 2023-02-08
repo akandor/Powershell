a603a6e2b6ce68c5fb2d217dedcbdca5da92185e
akandor <akandor@gmail.com>
2023-02-08 14:49:57 +0100

    Update While Input passed

--- Run-TeamsUserList.ps1
+++ Run-TeamsUserList.ps1
@@ -1,6 +1,7 @@
-$tenantID = "" #705088ec-e683-4174-b9bc-1920644dd49a
+$tenantID = "705088ec-e683-4174-b9bc-1920644dd49a" #705088ec-e683-4174-b9bc-1920644dd49a
 $localfile = ""
 $sheet = ""
+[uint16]$sheetID = 0
 
 Clear-Host
 
@@ -25,10 +26,28 @@ if($localfile -eq "") {
     }
     $null = $FileBrowser.ShowDialog()
     $localfile = $FileBrowser.FileName
+
+    if($localfile -eq "" -Or $localfile -eq $null) {
+        Write-Host "You need to select a file. Please restart the script!" -ForegroundColor Red
+        Break
+    }
 }
 
-$ExcelObj = New-Object -comobject Excel.Application
-$ExcelWorkBook = $ExcelObj.Workbooks.Open($localfile)
+try {
+    $ExcelObj = New-Object -comobject Excel.Application
+} catch {
+    Write-Host "Error while opening Excel. Please be sure that Excel is already installed and restart the script!" -ForegroundColor Red
+    $error[0].Exception
+    Break
+}
+
+try {
+    $ExcelWorkBook = $ExcelObj.Workbooks.Open($localfile)
+} catch {
+    Write-Host "Error while opening file. Please be sure that Excel is already installed and restart the script!" -ForegroundColor Red
+    $error[0].Exception
+    Break
+}
 
 function Get-Sheets {
     [array]$currentExcelWorkSheets = $ExcelWorkBook.Sheets
@@ -38,18 +57,23 @@ function Get-Sheets {
 
 Clear-Host
 
-Write-Host "Please choose a Workbook Sheet" -ForegroundColor Green
-$i=1
-$sheetList = Get-Sheets
-ForEach($sheetEntry in $sheetList) {
-If($sheetEntry.Name -eq $null -Or $sheetEntry.Name -eq "Data") {
-    continue
+while($sheetID -eq 0) {
+    Clear-Host
+    Write-Host "Please choose a Workbook Sheet" -ForegroundColor Green
+    $i=1
+    $sheetList = Get-Sheets
+    ForEach($sheetEntry in $sheetList) {
+    If($sheetEntry.Name -eq $null -Or $sheetEntry.Name -eq "Data") {
+        continue
+        }
+    Write-Host("["+ $sheetEntry.Index + "] " + $sheetEntry.Name)
     }
-Write-Host("["+ $sheetEntry.Index + "] " + $sheetEntry.Name)
+    $sheetID = Read-Host -Prompt "Choose"
 }
-[uint16]$sheetID = Read-Host -Prompt "Choose"
 
 $ExcelWorkSheet = $ExcelWorkBook.Sheets.Item($sheetID)
+
+Clear-Host
 
 $ii = 0
 
@@ -61,6 +85,8 @@ for($i=2;$i -le $ExcelWorkSheet.UsedRange.Rows.Count;$i++) {
     $vrp = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(5).Text)
     $moh = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(6).Text)
     $vm_lang = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(7).Text)
+
+
 
     $progress = 100 / ($ExcelWorkSheet.UsedRange.Rows.Count - 1) * ($ii++)
     $progressRounded = [math]::Round($progress)
@@ -77,7 +103,7 @@ for($i=2;$i -le $ExcelWorkSheet.UsedRange.Rows.Count;$i++) {
         Grant-CsTeamsFeedbackPolicy -Identity $user -PolicyName "Disable Survey Policy"
     }
     catch {
-        write-host "Phone Number $number cannot be added to $user" -ForegroundColor Yellow 
+        write-host "Phone Number $number cannot add to $user" -ForegroundColor Yellow 
         $error[0].Exception
         continue
     }
@@ -85,3 +111,7 @@ for($i=2;$i -le $ExcelWorkSheet.UsedRange.Rows.Count;$i++) {
 }
 
 $ExcelWorkBook.close()
+
+Clear-Host
+
+Read-Host -Prompt "All Done! Press Enter to quit!" 
