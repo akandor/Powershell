989863af08b37feddd26daaa652f5dab174e9c6a
akandor <akandor@gmail.com>
2023-02-09 12:08:59 +0100

    Adding Library

--- Run-TeamsUserList.ps1
+++ Run-TeamsUserList.ps1
@@ -2,6 +2,7 @@ $tenantID = "705088ec-e683-4174-b9bc-1920644dd49a" #705088ec-e683-4174-b9bc-1920
 $localfile = ""
 $sheet = ""
 [uint16]$sheetID = 0
+$global:progress = 0
 
 Clear-Host
 
@@ -63,7 +64,7 @@ while($sheetID -eq 0) {
     $i=1
     $sheetList = Get-Sheets
     ForEach($sheetEntry in $sheetList) {
-    If($sheetEntry.Name -eq $null -Or $sheetEntry.Name -eq "Data") {
+    If($null -eq $sheetEntry.Name -Or $sheetEntry.Name -eq "Data") {
         continue
         }
     Write-Host("["+ $sheetEntry.Index + "] " + $sheetEntry.Name)
@@ -75,32 +76,64 @@ $ExcelWorkSheet = $ExcelWorkBook.Sheets.Item($sheetID)
 
 Clear-Host
 
-$ii = 0
+$ii = 1
 
 for($i=2;$i -le $ExcelWorkSheet.UsedRange.Rows.Count;$i++) {
+
+    $global:progress = 100 / ($ExcelWorkSheet.UsedRange.Rows.Count - 1) * ($ii++)
+    $progressRounded = [math]::Round($global:progress)
+
     $user = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(1).Text)
     $number = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(2).Text)
     $cp = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(3).Text)
     $dp = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(4).Text)
     $vrp = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(5).Text)
     $moh = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(6).Text)
-    $vm_lang = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(7).Text)
+    $cpp = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(7).Text)
+    $cip = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(8).Text)
+    $emp = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(9).Text)
+    $vm_lang = ($ExcelWorkSheet.Rows.Item($i).Columns.Item(10).Text)
 
+    Write-Progress -Activity "Updating User $user" -Status "$progressRounded% Complete" -PercentComplete $global:progress
 
-
-    $progress = 100 / ($ExcelWorkSheet.UsedRange.Rows.Count - 1) * ($ii++)
-    $progressRounded = [math]::Round($progress)
-
-    Write-Progress -Activity "Updating User $user" -Status "$progressRounded% Complete" -PercentComplete $progress
+    if($ExcelWorkSheet.Rows.Item($i).Columns.Item(11).Text -eq "y") {
+        continue
+    }
 
     try {
-        Set-CsPhoneNumberAssignment -Identity $user -PhoneNumber $number -PhoneNumberType DirectRouting
-        Grant-CsTeamsCallingPolicy -Identity $user -PolicyName $cp
-        Grant-CsTenantDialPlan -Identity $user -PolicyName $dp
-        Grant-CsOnlineVoiceRoutingPolicy -Identity $user -PolicyName $vrp
-        Grant-CsTeamsCallHoldPolicy -Identity $user -PolicyName $moh
-        Set-CsOnlineVoicemailUserSettings -Identity $user -PromptLanguage $vm_lang
-        Grant-CsTeamsFeedbackPolicy -Identity $user -PolicyName "Disable Survey Policy"
+        if($user -ne "" -And $null -ne $user) {
+            if($number -ne "" -And $number -ne $null) {
+                Set-CsPhoneNumberAssignment -Identity $user -PhoneNumber $number -PhoneNumberType DirectRouting -ErrorActio Stop
+            }
+            if($cp -ne "" -And $cp -ne $null) {
+                Grant-CsTeamsCallingPolicy -Identity $user -PolicyName $cp -ErrorActio Stop
+            }
+            if($dp -ne "" -And $dp -ne $null) {
+                Grant-CsTenantDialPlan -Identity $user -PolicyName $dp -ErrorActio Stop
+            }
+            if($vrp -ne "" -And $vrp -ne $null) {
+                Grant-CsOnlineVoiceRoutingPolicy -Identity $user -PolicyName $vrp -ErrorActio Stop
+            }
+            if($moh -ne "" -And $moh -ne $null) {
+                Grant-CsTeamsCallHoldPolicy -Identity $user -PolicyName $moh -ErrorActio Stop
+            }
+            if($vm_lang -ne "" -And $vm_lang -ne $null) {
+                Set-CsOnlineVoicemailUserSettings -Identity $user -PromptLanguage $vm_lang -ErrorActio Stop
+            }
+            if($cpp -ne "" -And $cpp -ne $null) {
+                Grant-CsTeamsCallParkPolicy -Identity $user -PolicyName $cpp -ErrorActio Stop
+            }
+            if($cip -ne "" -And $cip -ne $null) {
+                Grant-CsCallingLineIdentity -Identity $user -PolicyName $cip -ErrorActio Stop
+            }
+            if($emp -ne "" -And $emp -ne $null) {
+                Grant-CsTeamsEmergencyCallingPolicy -Identity $user -PolicyName $emp -ErrorActio Stop
+            }
+            Grant-CsTeamsFeedbackPolicy -Identity $user -PolicyName "Disable Survey Policy" -ErrorActio Stop
+
+            $ExcelWorkSheet.Rows.Item($i).Columns.Item(11) = "y"
+        }
+        
     }
     catch {
         write-host "Phone Number $number cannot add to $user" -ForegroundColor Yellow 
@@ -110,8 +143,5 @@ for($i=2;$i -le $ExcelWorkSheet.UsedRange.Rows.Count;$i++) {
 
 }
 
-$ExcelWorkBook.close()
-
-Clear-Host
-
-Read-Host -Prompt "All Done! Press Enter to quit!" 
+$ExcelWorkBook.Save()
+$ExcelWorkBook.close($true)  
